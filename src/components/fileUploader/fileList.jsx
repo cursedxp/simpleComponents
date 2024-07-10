5003a09410f928e278a617f596896509f10c055c
Anil Ozsoy <anilozsoy@gmail.com>
2024-07-10 16:15:11 +0200

    ✨ (fileList.jsx): add support for uploading files and removing files from the file list 📝 (fileList.jsx): update comments and remove unused import ♻️ (fileList.jsx): refactor code to use useCallback for calculating file size and shortening file name ✨ (fileList.jsx): add upload button to allow users to upload files ♻️ (fileList.jsx): refactor code to use details prop instead of files prop ♻️ (fileList.jsx): refactor code to use IconButton component for removing files ♻️ (fileList.module.scss): refactor CSS to use flex-direction: column for the file list ♻️ (fileList.module.scss): refactor CSS to use justify-content: space-between and align-items: center for the header ♻️ (fileList.module.scss): refactor CSS to use font-size: 32px and font-weight: 700 for the header title

--- src/components/fileUploader/fileList.jsx
+++ src/components/fileUploader/fileList.jsx
@@ -1,32 +1,50 @@
 import PropTypes from "prop-types";
 import style from "./fileList.module.scss";
-import { StarIcon } from "@heroicons/react/24/solid";
+// import { StarIcon } from "@heroicons/react/24/solid";
+import { useCallback } from "react";
+import IconButton from "./iconButton";
+import { TrashIcon } from "@heroicons/react/24/outline";
+import UploadButton from "./uploadButton";
 
-export default function FileList({ files, className }) {
+export default function FileList({ details, className, removeFile }) {
+  const calculateFileSize = useCallback((size) => {
+    if (size === 0) return "0 Bytes";
+    const k = 1024;
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+    const i = Math.floor(Math.log(size) / Math.log(k));
+    return parseFloat((size / Math.pow(k, i)).toFixed(2)) + " " + sizes[i];
+  }, []);
+
+  const shortenFileName = useCallback((name) => {
+    if (name.length > 25) {
+      return name.slice(0, 15) + "...";
+    }
+    return name;
+  }, []);
+
   return (
     <div className={`${className}`}>
       <div className={style.header}>
         <div className={style.headerContext}>
           <h4>Files</h4>
-          <div className={style.headerInfo}>
+          {/* <div className={style.headerInfo}>
             <StarIcon className={style.icon} />
             <span>Please select concept preview image</span>
-          </div>
+          </div> */}
         </div>
+        <UploadButton text={"Upload"} onUpload={""} />
       </div>
       <ul>
-        {files.map((file, index) => (
+        {details.map((file, index) => (
           <li key={index}>
-            <img
-              src="https://images.unsplash.com/photo-1533779283484-8ad4940aa3a8"
-              alt=""
-              width={64}
-              height={64}
-            />
+            <img src={file.preview} alt="" width={64} height={64} />
             <div className={style.fileInfo}>
-              <p className={style.fileName}>{file.name}</p>
-              <p className={style.fileSize}>File size 12Mb </p>
+              <p className={style.fileName}>{shortenFileName(file.name)}</p>
+              <p className={style.fileSize}>{calculateFileSize(file.size)} </p>
             </div>
+            <IconButton onClick={() => removeFile(index)}>
+              <TrashIcon />
+            </IconButton>
           </li>
         ))}
       </ul>
@@ -35,6 +53,7 @@ export default function FileList({ files, className }) {
 }
 
 FileList.propTypes = {
-  files: PropTypes.arrayOf(PropTypes.object).isRequired,
+  details: PropTypes.arrayOf(PropTypes.object).isRequired,
   className: PropTypes.string,
+  removeFile: PropTypes.func.isRequired,
 };
